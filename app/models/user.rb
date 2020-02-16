645475405791cabcb1b7c4f7c444262d26b44fc0
Adrian082818 <mohabiradrian@gmail.com>
2020-02-16 09:50:05 -0500

    added attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password 
     has_many :movies
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true 
 end
